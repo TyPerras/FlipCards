818f78a08131c9bdda1573cef64054f2791ec6ec
TyPerras <93725009+TyPerras@users.noreply.github.com>
2022-09-02 13:56:58 -0400

    js-keyboard-inputs
    edits to allow keyboard inputs to flip cards

--- js/flipcards.js
+++ js/flipcards.js
@@ -1,3 +1,18 @@
-$('.flip-card').click(function() { 
-	$(this).toggleClass("flipped");
+// User selects a Flip Card using mouse "click" OR "enter" key on the keyboard
+// Pressing the Enter Key whilst focused on a button is treated in the same way as a click event by the browser
+$('.card-list-item').click(function(event) {
+	toggleCardFunction($(this));
 });
+
+// User selects a Flip Card using the "space" key on the keyboard
+$('.card-list-item').keypress(function(event) {
+	// Keycode 32 is the "space" key
+	if (event.keyCode == 32) {
+		toggleCardFunction($(this));
+	}
+});
+
+// Actual function to flip the card
+function toggleCardFunction(selectedCard) {
+	$(selectedCard).children('div.flip-card').toggleClass("flipped");
+}
